1f555425d44263136625122e7709a80834d609e1
SOARESGG10 <spectggof@gmail.com>
2023-04-05 23:50:01 -0300

    📑 | Create chess layer: ChessMatch and ChessPiece

---
+++ src/br/soares/chess/ChessMatch.java
@@ -0,0 +1,22 @@
+package br.soares.chess;
+
+import br.soares.boardgame.Board;
+
+public class ChessMatch {
+    private Board board;
+
+    public ChessMatch() {
+        board = new Board(8, 8);
+    }
+
+    public ChessPiece[][] getPieces() {
+        ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
+        for (int row = 0; row < board.getRows(); row++) {
+            for (int column = 0; column < board.getColumns(); column++) {
+                mat[row][column] = (ChessPiece) board.piece(row, column);
+            }
+        }
+        return mat;
+    }
+
+}
---
+++ src/br/soares/chess/enums/Color.java
@@ -0,0 +1,6 @@
+package br.soares.chess.enums;
+
+public enum Color {
+    BLACK,
+    WHITE
+}
